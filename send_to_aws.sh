4b5b1bd1ebd50acb412b4686342655acf6878ac2
ethan <e_posner@outlook.com>
2023-02-15 14:42:37 -0800

    Improves send_to_aws.sh to also send check_permissions.sh file

--- send_to_aws.sh
+++ send_to_aws.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-scp -r $(PWD)/{requirements.txt,Dockerfile,docker-compose.yml,.env,src/openai_discord_bot.py,src/openai_logger.py,check_permissions.sh} discord-api:~/discord-bot
+scp -r $(PWD)/{requirements.txt,Dockerfile,docker-compose.yml,.env,check_permissions.sh} discord-api:~/discord-bot
+scp -r $(PWD)/{src/openai_discord_bot.py,src/openai_logger.py} discord-api:~/discord-bot/src
